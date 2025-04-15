d1279cca33c837c48d7a2c1f3e4f9fbfc4cf9f46
Hiroyukiwk <delta1887rai@gmail.com>
2025-04-15 21:50:31 +0900

    修正

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,31 +1,23 @@
-import { getNewsList } from "@/app/_libs/microcms";
 import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-interface PageProps {
+type Props = {
   params: {
     slug: string;
   };
-  searchParams?: {
+  searchParams: {
     dk?: string;
   };
-}
-
-export async function generateStaticParams() {
-  const data = await getNewsList(); // 例：全ニュース記事取得
-  return data.contents.map((item) => ({
-    slug: item.id,
-  }));
-}
+};
 
 // test
-export default async function Page({ params, searchParams }: PageProps) {
+export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams?.dk,
-  }).catch(() => notFound());
+    draftKey: searchParams.dk,
+  }).catch(notFound);
   return (
     <>
       <Article data={data} />
