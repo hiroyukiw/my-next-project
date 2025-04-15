70eb36c1c72b5f75015c74f85461b12de9fb7d78
Hiroyukiwk <delta1887rai@gmail.com>
2025-04-15 20:49:41 +0900

    偏向

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,20 +4,20 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-type Props = {
+interface PageProps {
   params: {
     slug: string;
   };
-  searchParams: {
+  searchParams?: {
     dk?: string;
+    [key: string]: string | string[] | undefined;
   };
-};
+}
 
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({ params, searchParams }: PageProps) {
   const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams.dk,
+    draftKey: searchParams?.dk,
   }).catch(notFound);
-
   return (
     <>
       <Article data={data} />
