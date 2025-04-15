8b6f9ff8f55fc7efb6226f03a5779c88287e963e
Hiroyukiwk <delta1887rai@gmail.com>
2025-04-16 08:27:11 +0900

    変更

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,16 +4,22 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-type Props = {
-  params: {
-    slug: string;
-  };
-  searchParams: {
-    dk?: string;
-  };
-};
+// type Props = {
+//   params: {
+//     slug: string;
+//   };
+//   searchParams: {
+//     dk?: string;
+//   };
+// };
 
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { dk?: string };
+}) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
   }).catch(notFound);
