942024464420adfdf562f65cb25d9ad207d23372
Hiroyukiwk <delta1887rai@gmail.com>
2025-04-16 08:10:52 +0900

    直接

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,20 +4,23 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-type NewsDetailPageProps = {
-  params: {
-    slug: string;
-  };
-  searchParams: {
-    dk?: string;
-  };
-};
+// type NewsDetailPageProps = {
+//   params: {
+//     slug: string;
+//   };
+//   searchParams: {
+//     dk?: string;
+//   };
+// };
 
 // test
 export default async function Page({
   params,
   searchParams,
-}: NewsDetailPageProps) {
+}: {
+  params: { slug: string };
+  searchParams: { dk?: string };
+}) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
   }).catch(notFound);
