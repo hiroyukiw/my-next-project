83d986d7fa2b0c06af3b69622209f285bcc2d3ba
Hiroyukiwk <delta1887rai@gmail.com>
2025-04-16 22:08:20 +0900

    追加01

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -27,3 +27,7 @@ export default async function Page({ params, searchParams }: Props) {
     </>
   );
 }
+
+export async function generateStaticParams() {
+  return [] as Array<{ slug: string }>;
+}
